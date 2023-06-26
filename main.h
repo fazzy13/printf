0d8bef8645f6abbcc5df9cb571b8e5f964b698aa
Abu Jehoiada <abujehoiada@gmail.com>
2023-06-26 06:05:29 -0700

    Adjusted comment

--- main.h
+++ main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h> // for variable arguments
+#include <stdarg.h> /* for variable arguments */
 
 int _printf(const char *format, ...);
-
-int _putchar(char); /*For outputting characters */
-int _puts(char *); /*For output of Strings */
 
 #endif /* MAIN_H */

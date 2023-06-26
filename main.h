5aa27b45cc21fa477ec4439dba7add939dd7659c
fazzy13 <kaluifeanyi003@gmail.com>
2023-06-26 14:24:53 +0100

    add header file

--- main.h
+++ main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h> /* for variable arguments */
+#include <stdarg.h> /* for va_list */
 
+/* Function prototypes */
 int _printf(const char *format, ...);
+int _putchar(char c);
+int _puts(char *str);
 
 #endif /* MAIN_H */
+

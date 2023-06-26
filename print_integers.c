487a95a6b03d50335cbb3ef66237b7d12e8366dd
fazzy13 <kaluifeanyi003@gmail.com>
2023-06-27 00:03:27 +0100

    Implement print_integer and print_binary

--- print_integers.c
+++ print_integers.c
@@ -7,42 +7,53 @@
 void print_integer(int n)
 {
 	unsigned int num;
-	int last_digit;
-	int div;
+	int digit;
+	int is_negative = 0;
 
+	/* Handle special case for 0 */
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	/* Handle negative numbers */
 	if (n < 0)
 	{
-		_putchar('-');
+		is_negative = 1;
 		num = -n;
+		_putchar('-');
 	}
 	else
 	{
 		num = n;
 	}
 
-	div = 1;
+	/* Recursively print the digits */
+	if (num >= 10)
+		print_integer(num / 10);
 
-	while ((num / div) > 9)
-		div *= 10;
-
-	while (div != 0)
-	{
-		last_digit = num / div;
-		_putchar(last_digit + '0');
-		num %= div;
-		div /= 10;
-	}
+	/* Print the last digit */
+	digit = num % 10;
+	_putchar(digit + '0');
 }
 
 /**
- * print_binary - Prints a binary representation of an unsigned integer
+ * print_binary - Prints an unsigned integer in binary format
  * @n: The unsigned integer to print
  */
 void print_binary(unsigned int n)
 {
-	if (n > 1)
-		print_binary(n / 2);
+	unsigned int mask = 1;
+	int bits = sizeof(unsigned int) * 8;
+	int i;
 
-	_putchar((n % 2) + '0');
+	/* Iterate through each bit and print it */
+	for (i = bits - 1; i >= 0; i--)
+	{
+		/* Extract the bit using bitwise shifting and masking */
+		unsigned int bit = (n >> i) & mask;
+		_putchar(bit + '0');
+	}
 }
 

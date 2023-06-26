6185f6486cf5234d5f870c2f79770afc49cd9c38
fazzy13 <kaluifeanyi003@gmail.com>
2023-06-26 14:38:34 +0100

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -6,7 +6,8 @@ int _printf(const char *format, ...);
 /**
  * _printf - Printf function
  * @format: format.
- * Return: the number of characters printed (excluding the null byte used to end output to strings)
+ * Return: the number of characters printed (excluding the null byte used
+ * to end output to strings)
  */
 int _printf(const char *format, ...)
 {
@@ -39,11 +40,13 @@ int _printf(const char *format, ...)
 }
 
 /**
- * handle_print - Handles the conversion specifier and prints the corresponding output
+ * handle_print - Handles the conversion specifier and prints the
+ * corresponding output
  * @format: format string
  * @index: pointer to the current position in the format string
  * @list: argument list
- * Return: the number of characters printed for the current conversion specifier
+ * Return: the number of characters printed for the current
+ * conversion specifier
  */
 int handle_print(const char *format, int *index, va_list list)
 {
@@ -52,12 +55,14 @@ int handle_print(const char *format, int *index, va_list list)
 	if (format[*index] == 'c')
 	{
 		char c = va_arg(list, int);
-		_putchar(c);
+
+		putchar(c);
 		printed_chars++;
 	}
 	else if (format[*index] == 's')
 	{
 		char *str = va_arg(list, char *);
+
 		if (str == NULL)
 			str = "(null)";
 		printed_chars += _puts(str);

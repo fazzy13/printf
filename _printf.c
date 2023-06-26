518317db0bdf04f0819e295dbb1877e3d414a042
fazzy13 <kaluifeanyi003@gmail.com>
2023-06-26 14:30:18 +0100

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,109 +1,79 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h> /*  for write */
 
+int handle_print(const char *format, int *index, va_list list);
 int _printf(const char *format, ...);
-const char *process_conversion(const char *format, va_list args, int *printed_chars);
-void print_percent(int *printed_chars);
-void print_string(char *str, int *printed_chars);
-void print_char(char c, int *printed_chars);
 
 /**
- * print_char - Prints a single character to the standard output.
- * @c: The character to be printed.
- * @printed_chars: Pointer to the count of printed characters.
- */
-void print_char(char c, int *printed_chars)
-{
-    write(1, &c, 1);
-    (*printed_chars)++;
-}
-
-/**
- * print_string - Prints a string to the standard output.
- * @str: The string to be printed.
- * @printed_chars: Pointer to the count of printed characters.
- */
-void print_string(char *str, int *printed_chars)
-{
-    if (str == NULL)
-        str = "(null)";
-    while (*str)
-    {
-        write(1, str, 1);
-        str++;
-        (*printed_chars)++;
-    }
-}
-
-/**
- * print_percent - Prints a percent symbol to the standard output.
- * @printed_chars: Pointer to the count of printed characters.
- */
-void print_percent(int *printed_chars)
-{
-    write(1, "%", 1);
-    (*printed_chars)++;
-}
-
-/**
- * process_conversion - Processes a single conversion specifier.
- * @format: The format string.
- * @args: The variable argument list.
- * @printed_chars: Pointer to the count of printed characters.
- * Return: The updated position in the format string.
- */
-const char *process_conversion(const char *format, va_list args, int *printed_chars)
-{
-    switch (*format)
-    {
-        case 'c':
-            print_char(va_arg(args, int), printed_chars);
-            break;
-        case 's':
-            print_string(va_arg(args, char *), printed_chars);
-            break;
-        case '%':
-            print_percent(printed_chars);
-            break;
-        default:
-            print_percent(printed_chars);
-            print_char(*format, printed_chars);
-            break;
-    }
-
-    return format + 1;
-}
-
-/**
- * _printf - Produces output according to a format.
- * @format: The format string.
- * Return: The number of characters printed (excluding the null byte used to end output to strings).
+ * _printf - Printf function
+ * @format: format.
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int printed_chars = 0;
+	int i, printed_chars = 0;
+	va_list list;
 
-    va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++; // Move past '%'
-            format = process_conversion(format, args, &printed_chars);
-        }
-        else
-        {
-            print_char(*format, &printed_chars);
-        }
+	va_start(list, format);
 
-        format++;
-    }
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			printed_chars++;
+		}
+		else
+		{
+			i++;
+			/* Handle the conversion specifier using the helper function */
+			printed_chars += handle_print(format, &i, list);
+		}
+	}
 
-    va_end(args);
+	va_end(list);
 
-    return printed_chars;
+	return (printed_chars);
+}
+
+/**
+ * handle_print - Handles the conversion specifier and prints the corresponding output
+ * @format: format string
+ * @index: pointer to the current position in the format string
+ * @list: argument list
+ * Return: the number of characters printed for the current conversion specifier
+ */
+int handle_print(const char *format, int *index, va_list list)
+{
+	int printed_chars = 0;
+
+	if (format[*index] == 'c')
+	{
+		char c = va_arg(list, int);
+		_putchar(c);
+		printed_chars++;
+	}
+	else if (format[*index] == 's')
+	{
+		char *str = va_arg(list, char *);
+		if (str == NULL)
+			str = "(null)";
+		printed_chars += _puts(str);
+	}
+	else if (format[*index] == '%')
+	{
+		_putchar('%');
+		printed_chars++;
+	}
+	else
+	{
+		_putchar('%');
+		_putchar(format[*index]);
+		printed_chars += 2;
+	}
+
+	return (printed_chars);
 }
 
